6f68e3197ce4fc25caa43315dba849811967dd3c
rishabhravi1 <rravi3@uncc.edu>
2023-02-09 00:37:45 -0500

    Power Calculator update
    Almost done

--- Power Calculator/Power_Calculator.m
+++ Power Calculator/Power_Calculator.m
@@ -1,3 +1,4 @@
+clear
 %% Take inputs
 VehicleMass = 40.194277;
 VelocityTrace = csvread('EnduranceVelocityTrace.csv');
@@ -20,3 +21,8 @@ time_limits = [min(time), max(time)];
 time_index = time_limits(1) <= time & time <= time_limits(2);
 dx = time(2) - time(1); % Calculate the step size
 Energy = sum(Velocity(time_index)) * dx;
+
+disp(['The Energy required for 1 lap is: ', num2str(Energy), ' joules']);
+
+total_Energy = (Energy*13)/3600;
+disp(['Total Energy required to complete endurance is: ', num2str(total_Energy), ' watt-hours']);
